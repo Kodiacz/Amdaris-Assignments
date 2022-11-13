ac81c42eb72febd3f8e6ac38f645338ed84b8bd7
Simeon Yordanov <lookmanche@gmail.com>
2022-11-13 20:54:45 +0200

    pushing the assignment for ArraysAndGenerics

---
+++ ArraysAndGeneric/ArraysAndGenericAsignmentApp/ArraysAndGenericAsignment/ArrayList.cs
@@ -0,0 +1,80 @@
+﻿namespace ArraysAndGenericAsignment
+{
+    public class ArrayList<T>
+    {
+        T[] array;
+
+        public ArrayList()
+        {
+            array = new T[4];
+        }
+
+
+        public ArrayList(params T[] values)
+        {
+            array = new T[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                array[i] = values[i];
+            }
+        }
+        public T[] Array => this.array;
+
+        public int Length => this.array.Length;
+
+        public T GetItemAtIndex(int index) 
+        {
+            return array[index];
+        }
+
+        public void SetItemAtIndex(T item, int index)
+        {
+            T[] newArray = new T[this.Length + 1];
+            int oldArrayIndex = 0;
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (i == index)
+                {
+                    newArray[i] = item;
+                    i++;
+                }
+
+                newArray[i] = this.array[oldArrayIndex++];
+            }
+
+            this.array = newArray;
+        }
+
+        public void SwapItems(int firstIndex, int secondIndex)
+        {
+            T tempItem = this.array[firstIndex];
+            this.array[firstIndex] = this.array[secondIndex];
+            this.array[secondIndex] = tempItem;
+        }
+
+        public void SwapItems(T firstItem, T secondItem)
+        {
+            int indexOfFirstItem = 0;
+            int indexOfSecondItem = 0;
+
+            for (int i = 0; i < this.array.Length; i++)
+            {
+                if (this.array[i].Equals(firstItem))
+                {
+                    indexOfFirstItem = i;
+                }
+
+                if (this.array[i].Equals(secondItem))
+                {
+                    indexOfSecondItem = i;
+                }
+            }
+
+            T tempItem = this.array[indexOfFirstItem];
+            this.array[indexOfFirstItem] = this.array[indexOfSecondItem];
+            this.array[indexOfSecondItem] = tempItem;
+        }
+    }
+}
---
+++ ArraysAndGeneric/ArraysAndGenericAsignmentApp/ArraysAndGenericAsignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using ArraysAndGenericAsignment;
+
+
+string[] items = new string[]
+{
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+};
+
+ArrayList<string> arrayList = new ArrayList<string>(items);
+
+arrayList.SetItemAtIndex("2", 1);
+
+Console.WriteLine(string.Join(" ", arrayList.Array));
+
+arrayList.SwapItems(3, 4);
+
+Console.WriteLine(string.Join(" ", arrayList.Array));
+
+arrayList.SwapItems("1", "5");
+
+Console.WriteLine(string.Join(" ", arrayList.Array));
+
+Console.WriteLine(arrayList.GetItemAtIndex(5));
+
+
