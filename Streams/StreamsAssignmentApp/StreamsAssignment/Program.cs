05c26d0e2821280534a9101d42aed7d9cb828fcd
Simeon Yordanov <lookmanche@gmail.com>
2022-11-17 17:49:43 +0200

    commiting the assignment for streams

---
+++ Streams/StreamsAssignmentApp/StreamsAssignment/Program.cs
@@ -0,0 +1,11 @@
+﻿using static StreamsAssignment.TxtStream;
+using static System.Console;
+
+
+WriteLine("Type the name of the file:");
+WriteOnTxtFile();
+
+WriteLine("Pres enter to print the text from the file");
+ReadLine();
+ReadFromTxtFile();
+Compress();
---
+++ Streams/StreamsAssignmentApp/StreamsAssignment/TxtStream.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace StreamsAssignment
+{
+    public class TxtStream
+    {
+        public static void WriteOnTxtFile()
+        {
+            string input = Console.ReadLine();
+
+            if (!input.Contains(".txt"))
+            {
+                input += ".txt";
+            }
+
+            StreamWriter writer = new StreamWriter($@"C:\Simeon\Programming\Amdaris\Amdaris-Assignemnts-Git-Repo\Streams\{input}");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                string text = $"Line {i}";
+                writer.WriteLine(text);
+            }
+
+            writer.Close();
+        }
+
+        public static void ReadFromTxtFile()
+        {
+            StreamReader reader = StreamReader.Null;
+
+            try
+            {
+                reader = new StreamReader($@"C:\Simeon\Programming\Amdaris\Amdaris-Assignemnts-Git-Repo\Streams\test.txt");
+
+                for (var (line, lineNumber) = (reader.ReadLine(), 1); line != null; line = reader.ReadLine(), lineNumber++)
+                {
+                    Console.WriteLine($"{lineNumber}: {line}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        public static void Compress()
+        {
+            using Stream originalFile = File.Open($@"C:\Simeon\Programming\Amdaris\Amdaris-Assignemnts-Git-Repo\Streams\test.txt", FileMode.Open);
+            using Stream compressedFile = File.Create($@"C:\Simeon\Programming\Amdaris\Amdaris-Assignemnts-Git-Repo\Streams\compressedFile.gz");
+
+            using GZipStream gZipStream = new GZipStream(compressedFile, CompressionMode.Compress);
+
+            originalFile.CopyTo(gZipStream);
+
+            //originalFile.Close();
+            //compressedFile.Close();
+            //gZipStream.Close();
+        }
+
+
+    }
+}
