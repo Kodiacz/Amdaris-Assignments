105bd6fbefb23e3329f9e840d7663a40fdc804f4
Simeon Yordanov <lookmanche@gmail.com>
2022-12-02 20:51:44 +0200

    added servicec in Program.cs

--- PetClinicApp/PetClinic.Api/Program.cs
+++ PetClinicApp/PetClinic.Api/Program.cs
@@ -1,32 +1,35 @@
+using MediatR;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using PetClinic.Application.Interfaces;
 using PetClinic.Infrastructure;
+using PetClinic.Infrastructure.Repository;
+using PetClinic.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+builder.Services.AddScoped<IReceptionistRepository, ReceptionistRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+builder.Services.AddScoped<IPetRepository, PetRepository>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<PetClinicDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
-
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//builder.Services.AddDbContext<PetClinicDbContext>(options =>
-//{
-//    options.UseSqlServer(@"Server=LAPTOP-6HEN1T16\SQLEXPRESS;Database=MyDatabase;Trusted_Connection=True;");
-//});
-
+builder.Services.AddMediatR(typeof(Program));
+builder.Services.AddAutoMapper(typeof(Program));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,6 +37,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
