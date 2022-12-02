c8fda8401c2bb36572b35bda53f1c2e563647cb9
Simeon Yordanov <lookmanche@gmail.com>
2022-12-02 22:37:16 +0200

    created BaseController which contains Logger, Mediator, Mapper for more clean controllers

---
+++ PetClinicApp/PetClinic.Api/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿namespace PetClinic.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using AutoMapper;
+    using MediatR;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<T> : ControllerBase where T : BaseController<T>
+    {
+        private ILogger<T>? logger;
+        private IMapper? mapper;
+        private IMediator? mediator;
+
+        protected ILogger<T> Logger => logger ??= HttpContext.RequestServices.GetRequiredService<ILogger<T>>();
+
+        protected IMapper Mapper => mapper ??= HttpContext.RequestServices.GetRequiredService<IMapper>();
+
+        protected IMediator Madiator => mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+    }
+}
