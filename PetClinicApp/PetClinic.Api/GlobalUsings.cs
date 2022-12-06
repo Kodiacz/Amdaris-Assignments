398b2936ddcfa3a7f76815941070be6bdb6233b9
Simeon Yordanov <lookmanche@gmail.com>
2022-12-06 14:53:28 +0200

    created global usings for application layer

--- PetClinicApp/PetClinic.Api/GlobalUsings.cs
+++ PetClinicApp/PetClinic.Api/GlobalUsings.cs
@@ -1,5 +1,1 @@
 ﻿global using static PetClinic.Infrastructure.Common.DatabaseConstants.DoctorConstants;
-global using static PetClinic.Infrastructure.Common.DatabaseConstants.ReceptionistConstants;
-global using static PetClinic.Infrastructure.Common.DatabaseConstants.OwnerConstants;
-global using static PetClinic.Infrastructure.Common.DatabaseConstants.PetConstnats;
-global using static PetClinic.Infrastructure.Common.HelperMethods.PhoneGenerator;
