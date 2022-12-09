867882ea0aea3bb3eac1d19e40a3e2c266684953
Simeon Yordanov <lookmanche@gmail.com>
2022-12-09 09:46:40 +0200

    created owner dto for Get action

---
+++ PetClinicApp/PetClinic.Api/Dtos/OwnerDtos/GetOwnerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PetClinic.Api.Dtos.OwnerDtos
+{
+    public class GetOwnerDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Phonenumber { get; set; }
+    }
+}
