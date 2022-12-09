4ecb96e03e12c3ee39e14847cdd3839e9a5847a8
Simeon Yordanov <lookmanche@gmail.com>
2022-12-09 10:42:16 +0200

    coment unnused code to build the project

--- PetClinicApp/PetClinic.ConsolePresentation/Program.cs
+++ PetClinicApp/PetClinic.ConsolePresentation/Program.cs
@@ -9,20 +9,20 @@ var diContainer = new ServiceCollection()
 
 var mediator = diContainer.GetRequiredService<IMediator>();
 
-var userId = await mediator.Send(new CreateOwnerCommand()
-{
-    FirstName = "Stefan",
-    LastName = "Petrov",
-    Phonenumber = "0899443323",
-    Age = 33,
-});
+//var userId = await mediator.Send(new CreateOwnerCommand()
+//{
+//    FirstName = "Stefan",
+//    LastName = "Petrov",
+//    Phonenumber = "0899443323",
+//    Age = 33,
+//});
 
-var petId = await mediator.Send(new CreatePetCommand()
-{
-    Age = 2,
-    Breed = "dog",
-    Name = "Rex",
-    OwnerId = userId,
-});
+//var petId = await mediator.Send(new CreatePetCommand()
+//{
+//    Age = 2,
+//    Breed = "dog",
+//    Name = "Rex",
+//    OwnerId = userId,
+//});
 
 Console.WriteLine("Completed");
