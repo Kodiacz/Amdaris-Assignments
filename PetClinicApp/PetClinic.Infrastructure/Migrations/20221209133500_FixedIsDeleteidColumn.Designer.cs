77c182ce6b1bf5ca1209b267ed2b128a29f90faa
Simeon Yordanov <lookmanche@gmail.com>
2022-12-09 15:39:37 +0200

    new migration for fixing the IsDeleted properties

--- PetClinicApp/PetClinic.Infrastructure/Migrations/20221209133500_FixedIsDeleteidColumn.Designer.cs
+++ PetClinicApp/PetClinic.Infrastructure/Migrations/20221209133500_FixedIsDeleteidColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetClinic.Infrastructure;
 
@@ -11,9 +12,10 @@ using PetClinic.Infrastructure;
 namespace PetClinic.Infrastructure.Migrations
 {
     [DbContext(typeof(PetClinicDbContext))]
-    partial class PetClinicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221209133500_FixedIsDeleteidColumn")]
+    partial class FixedIsDeleteidColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PetClinicApp/PetClinic.Infrastructure/Migrations/20221209133500_FixedIsDeleteidColumn.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetClinic.Infrastructure.Migrations
+{
+    public partial class FixedIsDeleteidColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Phonenumber",
+                value: "0227899");
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Phonenumber",
+                value: "0233109");
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Phonenumber",
+                value: "0298904");
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Phonenumber",
+                value: "0288952");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Phonenumber",
+                value: "0884625784");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Phonenumber",
+                value: "0885691628");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Phonenumber",
+                value: "0893399705");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Phonenumber",
+                value: "0878470141");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Phonenumber",
+                value: "0215688");
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Phonenumber",
+                value: "0233727");
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Phonenumber",
+                value: "0251473");
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Phonenumber",
+                value: "0275727");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Phonenumber",
+                value: "0894556801");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Phonenumber",
+                value: "0876378080");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Phonenumber",
+                value: "0897484868");
+
+            migrationBuilder.UpdateData(
+                table: "Owners",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Phonenumber",
+                value: "0873441234");
+        }
+    }
+}
