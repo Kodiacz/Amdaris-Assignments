1dcb3d5b116d61f5f7743a76e6952fedba91585f
Simeon Yordanov <lookmanche@gmail.com>
2022-12-09 10:39:24 +0200

    created GetAllOwners action

--- PetClinicApp/PetClinic.Api/Controllers/OwnerController.cs
+++ PetClinicApp/PetClinic.Api/Controllers/OwnerController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace PetClinic.Api.Controllers
+﻿namespace PetClinic.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/owner")]
     [ApiController]
-    public class OwnerController : ControllerBase
+    public class OwnerController : BaseController<OwnerController>
     {
+        [HttpGet]
+        [Route("/GetAllOwners")]
+        public async Task<IActionResult> GetAllOwners()
+        {
+            GetAllOwners command = new GetAllOwners();
+            List<Owner> owners = await base.Madiator.Send(command);
+            List<GetOwnerDto> ownersDto = base.Mapper.Map<List<GetOwnerDto>>(owners);
+            return Ok(ownersDto);
+        }
     }
 }
