b383380812abaa0cefcb3b69b7f8d796aa1d6282
Simeon Yordanov <lookmanche@gmail.com>
2022-12-10 19:27:59 +0200

    created new dto for Create action

---
+++ PetClinicApp/PetClinic.Api/Dtos/OwnerDtos/CreateOwnerDto.cs
@@ -0,0 +1,22 @@
+﻿namespace PetClinic.Api.Dtos.OwnerDtos
+{
+    public class CreateOwnerDto
+    {
+        [Required]
+        [StringLength(FirstNameMaxLengthForOwner, MinimumLength = FirstNameMinLengthForOwner)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(LastNameMaxLengthForOwner, MinimumLength = LastNameMinLengthForOwner)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(AgeMinLengthForOwner, int.MaxValue)]
+        public int Age { get; set; }
+
+        [Phone]
+        [Required]
+        [StringLength(PhonenumberMaxLengthForOwner)]
+        public string Phonenumber { get; set; }
+    }
+}
