b8dfc17b4919b959be61d041b1651f00f5cd8ca8
Simeon Yordanov <lookmanche@gmail.com>
2022-12-11 12:45:12 +0200

    created more dtos for pets

--- PetClinicApp/PetClinic.Api/Dtos/PetDtos/CreatePetDto.cs
+++ PetClinicApp/PetClinic.Api/Dtos/PetDtos/CreatePetDto.cs
@@ -1,16 +1,12 @@
 ﻿namespace PetClinic.Api.Dtos.PetDtos
 {
-    public class GetPetDto
+    public class CreatePetDto
     {
-        public int Id { get; set; }
-
         public string Name { get; set; }
 
         public int Age { get; set; }
 
         public int OwnerId { get; set; }
-
-        public string OwnerFullName { get; set; }
 
         public string Breed { get; set; }
 
