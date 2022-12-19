caf1b1b9d8d77f872ccaab323ddc758a872385be
Simeon Yordanov <lookmanche@gmail.com>
2022-12-19 10:40:52 +0200

    added more usings

--- PetClinicApp/PetClinic.Test/Usings.cs
+++ PetClinicApp/PetClinic.Test/Usings.cs
@@ -9,11 +9,23 @@ global using Microsoft.EntityFrameworkCore;
 global using PetClinic.Application.Exceptions;
 global using PetClinic.Application.Interfaces;
 global using PetClinic.Infrastructure.Repository;
-global using PetClinic.Application.Owners.Queries.Get;
-global using PetClinic.Application.Owners.Commands.Create;
-global using PetClinic.Application.Owners.Commands.Delete;
-global using PetClinic.Application.Owners.Commands.Update;
+
+global using GetOwners = PetClinic.Application.Owners.Queries.Get;
+global using CreateOwners = PetClinic.Application.Owners.Commands.Create;
+global using DeleteOwners = PetClinic.Application.Owners.Commands.Delete;
+global using UpdateOwners = PetClinic.Application.Owners.Commands.Update;
+
 global using CreateDoctors = PetClinic.Application.Doctors.Commands.Create;
 global using UpdateDoctors = PetClinic.Application.Doctors.Commands.Update;
 global using DeleteDoctors = PetClinic.Application.Doctors.Commands.Delete;
 global using GetDoctors = PetClinic.Application.Doctors.Queries.Get;
+
+global using GetPets = PetClinic.Application.Pets.Queries.Get;
+global using CreatePets = PetClinic.Application.Pets.Commands.Create;
+global using DeletePets = PetClinic.Application.Pets.Commands.Delete;
+global using UpdatePets = PetClinic.Application.Pets.Commands.Update;
+
+global using GetRecepitionists = PetClinic.Application.Receptionists.Queries.Get;
+global using CreateRecepitionists = PetClinic.Application.Receptionists.Commands.Create;
+global using DeleteRecepitionists = PetClinic.Application.Receptionists.Commands.Delete;
+global using UpdateRecepitionists = PetClinic.Application.Receptionists.Commands.Update;
