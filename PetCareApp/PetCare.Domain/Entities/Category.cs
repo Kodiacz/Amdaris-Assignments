67120595305190daaa41566721c45099f48bf78c
Simeon Yordanov <lookmanche@gmail.com>
2022-12-27 21:45:14 +0200

    created 2 new entities in the domain layer

---
+++ PetCareApp/PetCare.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCare.Domain.Entities
+{
+    internal class Category
+    {
+    }
+}
