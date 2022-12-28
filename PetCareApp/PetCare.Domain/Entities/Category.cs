bd90c82bc8c44269b5be44ec860558be8a186cd3
Simeon Yordanov <lookmanche@gmail.com>
2022-12-28 21:30:32 +0200

    created properties for Product and Category

--- PetCareApp/PetCare.Domain/Entities/Category.cs
+++ PetCareApp/PetCare.Domain/Entities/Category.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PetCare.Domain.Entities
+﻿namespace PetCare.Domain.Entities
 {
-    internal class Category
+    public class Category
     {
+        public Category()
+        {
+            this.Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public ICollection<Product> Products { get; set; }
     }
 }
--- PetCareApp/PetCare.Domain/Entities/Product.cs
+++ PetCareApp/PetCare.Domain/Entities/Product.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PetCare.Domain.Entities
+﻿namespace PetCare.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public Category Category { get; set; } = null!;
     }
 }
