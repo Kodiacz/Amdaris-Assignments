b450684be8a76cd11d43b31c37f262d86ad58a97
Simeon Yordanov <lookmanche@gmail.com>
2023-01-06 00:19:32 +0200

    created Pet and PetList components

---
+++ PetCareApp/PetCare-FE/src/components/PetList.js
@@ -0,0 +1,37 @@
+import Pet from './Pet';
+import { useEffect, useState } from 'react';
+
+function PetList() {
+    const [pets, setDoctors] = useState([]);
+
+    useEffect(() => {
+        handleSubmit()
+    }, [])
+
+    async function handleSubmit() {
+        const resp = await fetch(`https://localhost:7038/api/Pet/GetAll`);
+        const data = await resp.json();
+        console.log(data);
+        setDoctors(data);
+    }
+
+    return (
+        <>
+            {/* <button type="button" onClick={handleSubmit}>Click here</button> */}
+            <div>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Owner</th>
+                        <th>Type</th>
+                        <th>Breed</th>
+                    </tr>
+                    {pets.map(p => <Pet key={p.id} pet={p} />)}
+                </table>
+            </div>
+        </>
+    );
+}
+
+export default PetList;
