0dcd5346aefbddb1d3706ea236c20425976cf89e
Simeon Yordanov <lookmanche@gmail.com>
2023-01-06 00:17:15 +0200

    working on extracting components

--- PetCareApp/PetCare-FE/src/App.js
+++ PetCareApp/PetCare-FE/src/App.js
@@ -2,47 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import DoctorsList from './components/DoctorsList.js'
 import HeadContactInfo from './components/HeadContactInfo';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    {/* //   <div className="container-fluid border-bottom d-none d-lg-block">
-    //     <div className="row gx-0">
-    //       <div className="col-lg-4 text-center py-2">
-    //         <div className="d-inline-flex align-items-center">
-    //           <i className="bi bi-geo-alt fs-1 text-primary me-3"></i>
-    //           <div className="text-start">
-    //             <h6 className="text-uppercase mb-1">Our Office</h6>
-    //             <span>123 Street, New York, USA</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div className="col-lg-4 text-center border-start border-end py-2">
-    //         <div className="d-inline-flex align-items-center">
-    //           <i className="bi bi-envelope-open fs-1 text-primary me-3"></i>
-    //           <div className="text-start">
-    //             <h6 className="text-uppercase mb-1">Email Us</h6>
-    //             <span>info@example.com</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div className="col-lg-4 text-center py-2">
-    //         <div className="d-inline-flex align-items-center">
-    //           <i className="bi bi-phone-vibrate fs-1 text-primary me-3"></i>
-    //           <div className="text-start">
-    //             <h6 className="text-uppercase mb-1">Call Us</h6>
-    //             <span>+012 345 6789</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div> */}
-
-
-      <HeadContactInfo />
-      <nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
+      {/* <nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
         <a href="index.html" className="navbar-brand ms-lg-5">
-          <h1 className="m-0 text-uppercase text-dark"><i className="bi bi-care fs-1 text-primary me-3"></i>Pet Care</h1>
+          <h1 className="m-0 text-uppercase text-dark"><i className="bi bi-shop fs-1 text-primary me-3"></i>Pet Care</h1>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
           <span className="navbar-toggler-icon"></span>
@@ -50,8 +17,9 @@ function App() {
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ms-auto py-0">
             <a href="index.html" className="nav-item nav-link active">Home</a>
-            <a href="about.html" className="nav-item nav-link">About</a>
-            <a href="service.html" className="nav-item nav-link">Service</a>
+            {/* <a href="about.html" className="nav-item nav-link">About</a> */}
+
+            {/* <a href="service.html" className="nav-item nav-link">Service</a>
             <a href="product.html" className="nav-item nav-link">Product</a>
             <div className="nav-item dropdown">
               <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
@@ -66,7 +34,7 @@ function App() {
             <a href="contact.html" className="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5">Contact <i className="bi bi-arrow-right"></i></a>
           </div>
         </div>
-      </nav>
+      </nav> */} 
 
 
       <div className="container-fluid bg-primary py-5 mb-5 hero-header">
