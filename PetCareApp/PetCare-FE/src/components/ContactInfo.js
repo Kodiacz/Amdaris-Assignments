f7376673561e5dad8367fdcfd4cb0e00aed8b4d1
Simeon Yordanov <lookmanche@gmail.com>
2023-01-05 16:33:29 +0200

    added brackets

--- PetCareApp/PetCare-FE/src/components/ContactInfo.js
+++ PetCareApp/PetCare-FE/src/components/ContactInfo.js
@@ -3,7 +3,8 @@ function ContactInfo({
     innerText,
     styleClassName
 }) {
-    return <div className="col-lg-4 text-center py-2">
+    return (
+    <div className="col-lg-4 text-center py-2">
         <div className="d-inline-flex align-items-center">
             <i className={`bi bi-${styleClassName} fs-1 text-primary me-3`}></i>
             <div className="text-start">
@@ -12,6 +13,6 @@ function ContactInfo({
             </div>
         </div>
     </div>
-};
+)};
 
 export default ContactInfo;
