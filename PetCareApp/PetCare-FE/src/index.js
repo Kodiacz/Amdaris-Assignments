bb63e1d21a7eb735006f42e5f1cc1c969c0405ee
Simeon Yordanov <lookmanche@gmail.com>
2023-01-08 01:00:38 +0200

    removed all components and added only App component

--- PetCareApp/PetCare-FE/src/index.js
+++ PetCareApp/PetCare-FE/src/index.js
@@ -3,31 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DoctorsList from './components/DoctorsList.js';
-import PetsList from './components/PetList.js'
-import NavBar from './components/NavBar.js';
-import HeadContactInfo from './components/HeadContactInfo.js';
 import FooterContactInfo from './components/FooterContactInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <BrowserRouter>
-      <HeadContactInfo />
-      <div className='main-body'>
-        <NavBar />
-        <Routes>
-          {/* <Route path="/somepage" element={<SomePage />} /> */}
-          <Route path="/doctors" element={<DoctorsList />} />
-          {/* <Route path="/about" element={<About />} /> */}
-          <Route path="/pets" element={<PetsList />} />
-          <Route path="/" element={<App />} />
-          {/* <Route path="*" element={<Page404 />} /> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <App />
     <FooterContactInfo />
   </React.StrictMode>
 );
