1d18b8450ef3163b92071fec9a49e629ae56f16f
Simeon Yordanov <lookmanche@gmail.com>
2023-01-09 16:43:49 +0200

    working on rendering new DoctorCard

--- PetCareApp/PetCare-FE/src/components/DoctorCard.js
+++ PetCareApp/PetCare-FE/src/components/DoctorCard.js
@@ -1,55 +1,10 @@
-// function Doctor({
-//     doctor,
-// }) {
-//     return (
-//         <>
-//             <tr>
-//                 <td>{doctor.firstName} {doctor.lastName}</td>
-//                 <td>{doctor.phonenumber}</td>
-//                 <td>{doctor.jobDescription}</td>
-//                 <td>{doctor.isAvailable ? 'Yes' : 'No'} </td>
-//             </tr>
-//         </>
-//     )
-// }
-
-// export default Doctor;
-
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-
-function DoctorCard({
+const DoctorCard = ({
     doctor
-}) {
+}) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {doctor.firstName} {doctor.lastName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
+    <div>
+      
+    </div>
   );
 }
 
