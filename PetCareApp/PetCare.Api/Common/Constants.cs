f3cb8cf25515046a71be59f2a28fa6c56b5adf56
Simeon Yordanov <lookmanche@gmail.com>
2023-01-09 16:52:22 +0200

    created common folder with cs file Constants
    created string constant for folder path

---
+++ PetCareApp/PetCare.Api/Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace PetCare.Api.Common
+{
+    static public class Constants
+    {
+        public const string ImagesFolderPath = @"C:\Simeon\Programming\Amdaris\Assignemnts-Git-Repo\PetCareApp\PetCare-FE\public\img\";
+    }
+}
