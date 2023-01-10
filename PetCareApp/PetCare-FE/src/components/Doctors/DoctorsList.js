fef9bd68be8a537de54e23f15fb7a17ce7e9fb8e
Simeon Yordanov <lookmanche@gmail.com>
2023-01-10 17:42:26 +0200

    working on implementing a loading component

--- PetCareApp/PetCare-FE/src/components/Doctors/DoctorsList.js
+++ PetCareApp/PetCare-FE/src/components/Doctors/DoctorsList.js
@@ -2,6 +2,7 @@ import './DoctorsList.css'
 import DoctorCard from './DoctorCard';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { CircularProgress } from '@mui/material';
 
 function DoctorsList() {
     const [doctors, setDoctors] = useState([]);
@@ -14,10 +15,10 @@ function DoctorsList() {
         const result = await axios.get(`https://localhost:7038/api/Doctor/GetAll`)
         setDoctors(result.data);
     }
-
+    const random = Math.random()
     return (
         <div className="doctor-list-container">
-            {doctors.map(d => <DoctorCard key={d.id} doctor={d} />)}
+            {random > 0.5 ? doctors.map(d => <DoctorCard key={d.id} doctor={d} />) : <CircularProgress />}
         </div>
     );
 }
