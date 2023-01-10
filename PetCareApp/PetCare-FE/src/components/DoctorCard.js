02ca9b8f4a4331d427d09d0e864f6a169d3dc003
Simeon Yordanov <lookmanche@gmail.com>
2023-01-10 17:01:53 +0200

    created new DoctorCard style and html structure

--- PetCareApp/PetCare-FE/src/components/DoctorCard.js
+++ PetCareApp/PetCare-FE/src/components/DoctorCard.js
@@ -1,9 +1,25 @@
 const DoctorCard = ({
-    doctor
+  doctor
 }) => {
+  
+  function configureImagePath(path) {
+    path = path.replace(`\\\\`, `\\`);
+    let index = path.indexOf(`img`);
+    path = path.slice(index, path.length + 1)
+    return path
+  }
+
+  const imagePath =configureImagePath(doctor.imageFilePath)
+  
   return (
-    <div>
-      
+    <div className="row-doctor-card-container">
+      <div className="col-doctor-card-container">
+        <div className="doctor-card-container">
+          <img src={imagePath} alt="Avatar" style={{ width: '280px', height: '280px' }} />
+            <h4 className="doctos-full-name"><b>{doctor.firstName} {doctor.lastName}</b></h4>
+            <button className="doctor-more-info-btn" type="button">Read more</button>
+        </div>
+      </div>
     </div>
   );
 }
