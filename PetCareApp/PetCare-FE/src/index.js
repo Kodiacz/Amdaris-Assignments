ba87dacfe71c72c64226e89ed74f1f91166f2025
Simeon Yordanov <lookmanche@gmail.com>
2023-01-10 17:42:51 +0200

    modified the path to the components

--- PetCareApp/PetCare-FE/src/index.js
+++ PetCareApp/PetCare-FE/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FooterContactInfo from './components/FooterContactInfo';
+import FooterContactInfo from './components/Common/FooterContactInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
