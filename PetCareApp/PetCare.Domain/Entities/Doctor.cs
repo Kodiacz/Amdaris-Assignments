e732d3380570f66ce7e573b4dc1f4c990aebdd04
Simeon Yordanov <lookmanche@gmail.com>
2023-01-10 17:05:56 +0200

    added new properties for ImageFilePath

--- PetCareApp/PetCare.Domain/Entities/Doctor.cs
+++ PetCareApp/PetCare.Domain/Entities/Doctor.cs
@@ -12,6 +12,8 @@
         
         public bool IsDeleted { get; set; } = false;
 
+        public string? ImageFilePath { get; set; }
+
         public ICollection<Pet> Patients { get; set; }
 
         public ICollection<Owner> OwnersOfPatients { get; set; }
--- PetCareApp/PetCare.Domain/Entities/Owner.cs
+++ PetCareApp/PetCare.Domain/Entities/Owner.cs
@@ -13,6 +13,8 @@
 
         public bool IsDeleted { get; set; } = false;
 
+        public string? ProfileImageFilePath { get; set; }
+
         public int? DoctorId { get; set; }
         public Doctor? Doctor { get; set; }
 
