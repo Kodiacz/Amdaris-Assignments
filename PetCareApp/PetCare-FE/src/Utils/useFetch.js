5ba21bdfdfb054d9c1edbfad15c01eafb71d5da3
Simeon Yordanov <lookmanche@gmail.com>
2023-01-11 10:05:52 +0200

    exported fetch function in file

--- PetCareApp/PetCare-FE/src/Utils/useFetch.js
+++ PetCareApp/PetCare-FE/src/Utils/useFetch.js
@@ -1,3 +1,27 @@
-export default useFetch = () => {
-    
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+    const [state, setState] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, [url])
+
+    async function fetchData() {
+        const result = await axios.get(url)
+            .catch(error => setError(error))
+        setIsLoading(false)
+        setState(result.data);
+    }
+
+    return {
+        state,
+        error,
+        isLoading,
+    }
 }
+
+export { useFetch }
