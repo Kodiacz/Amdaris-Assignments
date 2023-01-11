5a7b302ac4dbff7620e0fe1c2186fab817da22e4
Simeon Yordanov <lookmanche@gmail.com>
2023-01-11 12:29:12 +0200

    exported function for logic separation

--- PetCareApp/PetCare-FE/src/components/Doctors/DoctorCard.js
+++ PetCareApp/PetCare-FE/src/components/Doctors/DoctorCard.js
@@ -1,23 +1,17 @@
+import { configureImagePath } from "../../Utils/configurePath";
+
 const DoctorCard = ({
   doctor
 }) => {
-  
-  function configureImagePath(path) {
-    path = path.replace(`\\\\`, `\\`);
-    let index = path.indexOf(`img`);
-    path = path.slice(index, path.length + 1)
-    return path
-  }
+  const imagePath = configureImagePath(doctor.imageFilePath)
 
-  const imagePath =configureImagePath(doctor.imageFilePath)
-  
   return (
     <div className="row-doctor-card-container">
       <div className="col-doctor-card-container">
         <div className="doctor-card-container">
           <img src={imagePath} alt="Avatar" style={{ width: '280px', height: '280px' }} />
-            <h4 className="doctos-full-name"><b>{doctor.firstName} {doctor.lastName}</b></h4>
-            <button className="doctor-more-info-btn" type="button">Read more</button>
+          <h4 className="doctos-full-name"><b>{doctor.firstName} {doctor.lastName}</b></h4>
+          <button className="doctor-more-info-btn" type="button">Read more</button>
         </div>
       </div>
     </div>
