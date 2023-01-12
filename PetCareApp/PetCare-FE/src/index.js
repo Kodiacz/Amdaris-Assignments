bac2fd1f4c1abdf966d4d32080b6aebdb4856ac3
Simeon Yordanov <lookmanche@gmail.com>
2023-01-12 21:05:12 +0200

    added ./ to the stylesheet path

--- PetCareApp/PetCare-FE/src/index.js
+++ PetCareApp/PetCare-FE/src/index.js
@@ -7,10 +7,10 @@ import FooterContactInfo from './components/Common/FooterContactInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
     <FooterContactInfo />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
