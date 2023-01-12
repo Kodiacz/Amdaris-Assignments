0e3436c37323393d538f18bca72449110014d4c5
Simeon Yordanov <lookmanche@gmail.com>
2023-01-12 21:10:22 +0200

    minor commit

--- PetCareApp/PetCare-FE/src/components/Doctors/DoctorsList.js
+++ PetCareApp/PetCare-FE/src/components/Doctors/DoctorsList.js
@@ -2,17 +2,14 @@ import './DoctorsList.css'
 import DoctorCard from './DoctorCard';
 import { CircularProgress } from '@mui/material';
 import { GetAllDoctors } from './DoctorsServices.js';
+import { Outlet } from 'react-router-dom';
 
 function DoctorsList() {
-    const { state: doctors, isLoading, error } = GetAllDoctors();
-
-    console.log(doctors)
-    console.log(isLoading)
-    console.log(error)
+    const { state: doctors, isLoading } = GetAllDoctors();
 
     return (
         <div className="doctor-list-container">
-            {isLoading
+            { isLoading
                 ? <div className="doctors-load">
                     <CircularProgress color='success' size={120} thickness={1} />
                 </div>
