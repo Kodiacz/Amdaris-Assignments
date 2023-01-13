18bd03bb18e0fe6502c6e30b587965ac2c3889cd
Simeon Yordanov <lookmanche@gmail.com>
2023-01-13 16:05:28 +0200

    created a new class for the database named Schedule

---
+++ PetCareApp/PetCare.Domain/Entities/Schedule.cs
@@ -0,0 +1,14 @@
+﻿namespace PetCare.Domain.Entities
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; } = null!;
+    }
+}
