7319380d6541808cd63b02a738d03848a8bd3661
Simeon Yordanov <lookmanche@gmail.com>
2023-01-14 11:27:57 +0200

    created Schedule dtos folder and profile folder

---
+++ PetCareApp/PetCare.Api/DtoProfiles/ScheduleProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace PetCare.Api.DtoProfiles
+{
+    public class ScheduleProfile : Profile
+    {
+        public ScheduleProfile()
+        {
+            CreateMap<Schedule, GetScheduleDto>()
+                .ForMember(gsd => gsd.FullName, opt => opt.MapFrom(src => src.Doctor.FirstName + " " + src.Doctor.LastName));
+        }
+    }
+}
---
+++ PetCareApp/PetCare.Api/Dtos/ScheduleDtos/GetScheduleDto.cs
@@ -0,0 +1,15 @@
+﻿namespace PetCare.Api.Dtos.ScheduleDtos
+{
+    public class GetScheduleDto
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        public int DoctorId { get; set; }
+
+        public string FullName { get; set; } = null!;
+    }
+}
