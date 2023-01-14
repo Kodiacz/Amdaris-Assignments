2d7768fd039e85756336f23006ddcf6ed4c14520
Simeon Yordanov <lookmanche@gmail.com>
2023-01-14 11:43:02 +0200

    added 6 new constants for owner entity

--- PetCareApp/PetCare.Infrastructure/Common/DatabaseConstants/OwnerConstants.cs
+++ PetCareApp/PetCare.Infrastructure/Common/DatabaseConstants/OwnerConstants.cs
@@ -14,5 +14,14 @@ namespace PetCare.Infrastructure.Common.DatabaseConstants
         public const int AgeMinLengthForOwner = 1;
 
         public const int PhonenumberMaxLengthForOwner = 20;
+
+        public const int UserNameMaxLenghtForOwner = 30;
+        public const int UserNameMinLenghtForOwner = 3;
+
+        public const int EmailMaxLenghtForOwner = 40;
+        public const int EmailMinLenghtForOwner = 5;
+
+        public const int PasswordMaxLenghtForOwner = 35;
+        public const int PasswordMinLenghtForOwner = 4;
     }
 }
