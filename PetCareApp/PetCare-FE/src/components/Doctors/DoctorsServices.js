3dd511b31bafbb6b3b2f419f3943d4110d0eef54
Simeon Yordanov <lookmanche@gmail.com>
2023-01-14 11:23:24 +0200

    added new function GetDctorsScedule

--- PetCareApp/PetCare-FE/src/components/Doctors/DoctorsServices.js
+++ PetCareApp/PetCare-FE/src/components/Doctors/DoctorsServices.js
@@ -1,11 +1,19 @@
 import { useFetch } from "../../Utils/useFetch.js";
 
 function GetAllDoctors() {
-    return useFetch('https://localhost:7038/api/Doctor/GetAll')
+    return useFetch('https://localhost:7038/api/Doctor/GetAll');
 }
 
 function GetDoctor(doctorId) {
-    return useFetch(`https://localhost:7038/api/Doctor/GetById/${doctorId}`)
+    return useFetch(`https://localhost:7038/api/Doctor/GetById/${doctorId}`);
 }
 
-export { GetAllDoctors, GetDoctor }
+function GetDoctorSchedule(doctorId) {
+    return useFetch(`https://localhost:7038/api/Doctor/GetScheduleByDoctorId/${doctorId}`);
+}
+
+export { 
+    GetAllDoctors, 
+    GetDoctor,
+    GetDoctorSchedule, 
+};
