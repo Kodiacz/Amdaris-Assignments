77e879b0dee10e1653a7fde1c09eaaaededc2aad
Simeon Yordanov <lookmanche@gmail.com>
2023-01-15 22:35:06 +0200

    created ComputedPassword type to store a result in it

---
+++ PetCareApp/PetCare.Application/Models/ComputedPassword.cs
@@ -0,0 +1,9 @@
+﻿namespace PetCare.Application.Models
+{
+    public class ComputedPassword
+    {
+        public byte[] PasswordHash { get; set; } = null!; 
+
+        public byte[] PasswordSalt { get; set; } = null!;
+    }
+}
