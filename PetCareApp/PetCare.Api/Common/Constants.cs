5881912c3aa8477b062f0c4dfa6e3b4a0ae0d8a3
Simeon Yordanov <lookmanche@gmail.com>
2023-01-15 22:28:50 +0200

    created new consant for UserPicturesFolderPath

--- PetCareApp/PetCare.Api/Common/Constants.cs
+++ PetCareApp/PetCare.Api/Common/Constants.cs
@@ -2,6 +2,9 @@
 {
     static public class Constants
     {
+        public const string UserProfilePicturesFolderPath = 
+            @"C:\Simeon\Programming\Amdaris\Assignemnts-Git-Repo\PetCareApp\PetCare-FE\public\img\users-profile-images\";
+
         public const string ImagesFolderPath = @"C:\Simeon\Programming\Amdaris\Assignemnts-Git-Repo\PetCareApp\PetCare-FE\public\img\";
     }
 }
