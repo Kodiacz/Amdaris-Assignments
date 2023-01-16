59996b5bf3d0137ac756f6f989f054de5adaca65
Simeon Yordanov <lookmanche@gmail.com>
2023-01-16 17:40:51 +0200

    created command and handler for verifieing the user password

---
+++ PetCareApp/PetCare.Application/Authentication/Commands/VerifyPasswordHash.cs
@@ -0,0 +1,11 @@
+﻿namespace PetCare.Application.Authentication.Commands
+{
+    public class VerifyPasswordHash : IRequest<bool>
+    {
+        public string Password { get; set; } = null!;
+
+        public byte[] PasswordHash { get; set; } = null!;
+
+        public byte[] PasswordSalt { get; set; } = null!;
+    }
+}
---
+++ PetCareApp/PetCare.Application/Authentication/Commands/VerifyPasswordHashHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace PetCare.Application.Authentication.Commands
+{
+    public class VerifyPasswordHashHandler : IRequestHandler<VerifyPasswordHash, bool>
+    {
+        public async Task<bool> Handle(VerifyPasswordHash request, CancellationToken cancellationToken)
+        {
+            using (var hmac = new HMACSHA512(request.PasswordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(request.Password));
+                return computedHash.SequenceEqual(request.PasswordHash);
+            }
+        }
+    }
+}
