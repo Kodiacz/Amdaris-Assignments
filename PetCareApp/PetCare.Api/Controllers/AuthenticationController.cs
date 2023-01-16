9d0d8b934099b0d860fa107abaa9727a1ea5d543
Simeon Yordanov <lookmanche@gmail.com>
2023-01-16 17:37:18 +0200

    created login endpoint

--- PetCareApp/PetCare.Api/Controllers/AuthenticationController.cs
+++ PetCareApp/PetCare.Api/Controllers/AuthenticationController.cs
@@ -1,6 +1,7 @@
 ﻿namespace PetCare.Api.Controllers
 {
     using CreateOwner;
+    using GetOwner;
 
     [ApiController]
     [EnableCors("PetCare-FE")]
@@ -10,24 +11,39 @@
         [HttpPost]
         [ModelValidationFilter]
         [ActionName(nameof(Register))]
-        public async Task<ActionResult<Owner>> Register(CreateOwnerDto request)
+        public async Task<ActionResult<Owner>> Register(CreateOwnerDto registerRequest)
         {
-            CreatePasswordHash createPasswordHashCommand = new() { Password = request.Password };
+            CreatePasswordHash createPasswordHashCommand = new() { Password = registerRequest.Password };
             ComputedPassword computedPassowrd = await base.Mediator.Send(createPasswordHashCommand);
-            CreateOwner createOwnerCommand = base.Mapper.Map<CreateOwner>(request);
+            CreateOwner createOwnerCommand = base.Mapper.Map<CreateOwner>(registerRequest);
             createOwnerCommand.PasswordHash = computedPassowrd.PasswordHash;
             createOwnerCommand.PasswordSalt = computedPassowrd.PasswordSalt;
             Owner owner = await base.Mediator.Send(createOwnerCommand);
             return Ok(owner);
         }
 
-        //private void CreatePasswordHash(string password, out byte[] PasswordHash, out byte[] PasswordSalt)
-        //{
-        //    using (var hmac = new HMACSHA512())
-        //    {
-        //        PasswordSalt = hmac.Key;
-        //        PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        //    }
-        //}
+        [HttpPost]
+        public async Task<ActionResult<string>> Login(UserLoginDto loginRequest)
+        {
+            GetOwnerByUsername getOwnerCommand = new() { Username = loginRequest.Username };
+            Owner owner = await base.Mediator.Send(getOwnerCommand);
+            VerifyPasswordHash verifyPasswordCommand = new() 
+            {
+                Password = loginRequest.Password,
+                PasswordHash = owner.PasswordHash!,
+                PasswordSalt = owner.PasswordSalt!,
+            };
+
+            bool verification = await base.Mediator.Send(verifyPasswordCommand);
+
+            if (!verification)
+            {
+                return BadRequest("Wrong password");
+            }
+
+            GenerateToken generateTokenCommand = new() { Owner = owner };
+            string ownerToken = await this.Mediator.Send(generateTokenCommand);
+            return Ok(ownerToken);
+        }
     }
 }
