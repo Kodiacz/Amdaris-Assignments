e9be6117b1a9bc0b6bbd494da2bbb893154edc3a
Simeon Yordanov <lookmanche@gmail.com>
2023-01-17 20:10:40 +0200

    added AuthContext

--- PetCareApp/PetCare-FE/src/App.js
+++ PetCareApp/PetCare-FE/src/App.js
@@ -2,36 +2,47 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import './App.css';
+import { useState } from 'react';
 import Home from './components/Common/Home';
+import Login from './components/Login/Login';
 import About from './components/Common/About';
 import NavBar from './components/Common/NavBar';
-import DoctorsList from './components/Doctors/DoctorsList';
-import PetsList from './components/Pets/PetsList';
-import HeadContactInfo from './components/Common/HeadContactInfo';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Calendar from './components/Calendar/Calendar';
 import Doctor from './components/Doctors/Doctor';
-import Login from './components/Login/Login';
-import LoginModal from './components/Login/LoginModal';
+import PetsList from './components/Pets/PetsList';
+import { AuthContext } from './contexts/AuthContext';
+import Calendar from './components/Calendar/Calendar';
+import DoctorsList from './components/Doctors/DoctorsList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HeadContactInfo from './components/Common/HeadContactInfo';
 
 function App() {
+  const [user, setUser] = useState({
+    accessTokent: 'aksesche',
+  })
+
+  const login = (authData) => {
+    setUser(authData)
+  }
+
   return (
-    <BrowserRouter>
-      <HeadContactInfo />
-      <div className='main-body'>
-        <NavBar />
-        <Routes>
-          <Route path="/doctors/*" element={<DoctorsList />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/doctors/doctor/:id" element={<Doctor />} />
-          <Route path="/pets" element={<PetsList />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="*" element={<Page404 />} /> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthContext.Provider value={{user, login}}>
+      <BrowserRouter>
+        <HeadContactInfo />
+        <div className='main-body'>
+          <NavBar />
+          <Routes>
+            <Route path="/doctors/*" element={<DoctorsList />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/doctors/doctor/:id" element={<Doctor />} />
+            <Route path="/pets" element={<PetsList />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="*" element={<Page404 />} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
