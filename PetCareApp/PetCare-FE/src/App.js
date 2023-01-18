cfad4c16434cadb5bd55ebb7e94fc4350c4bfa8d
Simeon Yordanov <lookmanche@gmail.com>
2023-01-18 12:08:45 +0200

    wokring on error page and created login and logout functionality

--- PetCareApp/PetCare-FE/src/App.js
+++ PetCareApp/PetCare-FE/src/App.js
@@ -12,31 +12,45 @@ import PetsList from './components/Pets/PetsList';
 import { AuthContext } from './contexts/AuthContext';
 import Calendar from './components/Calendar/Calendar';
 import DoctorsList from './components/Doctors/DoctorsList';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import HeadContactInfo from './components/Common/HeadContactInfo';
+import ErrorPage from './Error/ErrorPage';
+import { toast, ToastContainer } from 'react-toastify';
+import useLocalStorage from './Hooks/useLocalStorage'
+
+const initialAuthState = {
+  accessToken: '',
+  username: '',
+  userId: '',
+};
 
 function App() {
-  const [user, setUser] = useState({
-    accessTokent: 'aksesche',
-  })
-
-  const login = (authData) => {
+  const [user, setUser] = useLocalStorage('user', initialAuthState)
+  
+  const onLogin = (authData) => {
+    console.log('onLogin', authData)
     setUser(authData)
+    toast("You were loged in");
+  }
+
+  const onLogout = () => {
+    setUser(initialAuthState);
   }
 
   return (
-    <AuthContext.Provider value={{user, login}}>
+    <AuthContext.Provider value={{ user, onLogin, onLogout }}>
       <BrowserRouter>
         <HeadContactInfo />
+        <ToastContainer />
         <div className='main-body'>
-          <NavBar />
+          <NavBar username={user.username} onLogout={onLogout}/>
           <Routes>
             <Route path="/doctors/*" element={<DoctorsList />} />
             <Route path="/about" element={<About />} />
             <Route path="/doctors/doctor/:id" element={<Doctor />} />
-            <Route path="/pets" element={<PetsList />} />
+            <Route path="/pets" element={<PetsList />} errorElement={<ErrorPage />}/>
             <Route path="/" element={<Home />} />
-            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/calendar" element={<Calendar />} errorElement={<ErrorPage />}/>
             <Route path="/login" element={<Login />} />
             {/* <Route path="*" element={<Page404 />} /> */}
           </Routes>
