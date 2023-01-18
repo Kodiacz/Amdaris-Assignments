ded866db7a928bffd31e7baa415e6c779e85e4f2
Simeon Yordanov <lookmanche@gmail.com>
2023-01-18 12:12:27 +0200

    added Username and UserId property

--- PetCareApp/PetCare.Application/Models/JwtToken.cs
+++ PetCareApp/PetCare.Application/Models/JwtToken.cs
@@ -2,6 +2,10 @@
 {
     public class JwtToken
     {
-        public string AccessToken { get; set; }
+        public string AccessToken { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+        public int UserId { get; set; } 
     }
 }
