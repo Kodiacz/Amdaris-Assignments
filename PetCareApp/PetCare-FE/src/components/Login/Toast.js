325230d4e17f97ec37b8514a76150105283af9b1
Simeon Yordanov <lookmanche@gmail.com>
2023-01-18 12:10:39 +0200

    created Toast

---
+++ PetCareApp/PetCare-FE/src/components/Login/Toast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+  import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+  
+  function Toast(){
+
+    return (
+      <div>
+        <ToastContainer />
+      </div>
+    );
+  }
+
+  export default Toast;
