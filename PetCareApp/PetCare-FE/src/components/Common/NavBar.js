b2326a6babb436c3f876b23b9df212748f2b385c
Simeon Yordanov <lookmanche@gmail.com>
2023-01-18 12:09:14 +0200

    crated separate html for user and guest

--- PetCareApp/PetCare-FE/src/components/Common/NavBar.js
+++ PetCareApp/PetCare-FE/src/components/Common/NavBar.js
@@ -2,8 +2,27 @@
 import LoginModal from '../Login/LoginModal';
 import { Link, NavLink } from 'react-router-dom'
 import LoginIcon from '@mui/icons-material/Login';
+import Toastche from '../Login/Toast';
 
-function NavBar() {
+function NavBar({
+    username,
+    onLogout,
+}) {
+    console.log(username);
+    let guestNavigation = (
+        <div className='guest-div'>
+            <a className="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5 login-logout"><LoginModal buttonText="Sign In" /></a>
+        </div>
+    );
+
+    let userNavigation = (
+        <div className='user-div'>
+            <h5>Welcome, {username}</h5>
+            <a className="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5 login-logout" onClick={onLogout}>Logout</a>
+        </div>
+    )
+
+
     return (
         <nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
             <a href="/" className="navbar-brand ms-lg-5">
@@ -14,17 +33,22 @@ function NavBar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
                 <div className="navbar-nav ms-auto py-0">
+                    <Toastche></Toastche>
                     <NavLink to="/about" activeClassName="active" className="nav-item nav-link">About</NavLink>
                     <NavLink to="/" activeClassName="active" className="nav-item nav-link">Home</NavLink>
                     <NavLink to="/shop" activeClassName="active" className="nav-item nav-link">Shop</NavLink>
                     <div className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                         <div className="dropdown-menu m-0">
-                            <Link to="pets" className="dropdown-item">Pets</Link>
+                            {username ? <Link to="pets" className="dropdown-item">Pets</Link> : ""}
                             <Link to="doctors" className="dropdown-item">Doctors Теам</Link>
                         </div>
                     </div>
-                    <a className="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5"><LoginModal buttonText="Sign In"/></a>
+                    {
+                        username
+                            ? userNavigation
+                            : guestNavigation
+                    }
                 </div>
             </div>
         </nav>
