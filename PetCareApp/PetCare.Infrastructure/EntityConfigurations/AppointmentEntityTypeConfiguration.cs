934c3035ac1b942b1159783ab3df31bd2e93fb5e
Simeon Yordanov <lookmanche@gmail.com>
2023-01-19 09:44:39 +0200

    created configuration file for Appointment Entity

---
+++ PetCareApp/PetCare.Infrastructure/EntityConfigurations/AppointmentEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace PetCare.Infrastructure.EntityConfigurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using PetCare.Domain.Entities;
+    using static PetCare.Infrastructure.Common.DatabaseConstants.AppointmentConstants;
+
+    public class AppointmentEntityTypeConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder
+                .HasKey(appointment => appointment.Id);
+
+            builder
+                .Property(appointment => appointment.ReasonForAppointment)
+                .HasMaxLength(ReasonForAppointmentMaxLength);
+
+            builder
+                .HasOne(appointment => appointment.Owner)
+                .WithMany(appointment => appointment.Appointments)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
