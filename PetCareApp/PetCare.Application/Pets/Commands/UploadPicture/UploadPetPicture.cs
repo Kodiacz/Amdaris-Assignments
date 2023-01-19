c053bff792aaaaee8d2192da3f4d0e4b0a9056c6
Simeon Yordanov <lookmanche@gmail.com>
2023-01-19 16:59:36 +0200

    created command and handler

---
+++ PetCareApp/PetCare.Application/Pets/Commands/UploadPicture/UploadPetPicture.cs
@@ -0,0 +1,9 @@
+﻿namespace PetCare.Application.Pets.Commands.UploadPicture
+{
+    public class UploadPetPicture : IRequest<string>
+    {
+        public string FolderPath { get; set; } = null!;
+
+        public string FileName { get; set; } = null!;
+    }
+}
---
+++ PetCareApp/PetCare.Application/Pets/Commands/UploadPicture/UploadPetPictureHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace PetCare.Application.Pets.Commands.UploadPicture
+{
+    public class UploadPetPictureHandler : IRequestHandler<UploadPetPicture, string>
+    {
+        private readonly IUnitOfWork repo;
+
+        public UploadPetPictureHandler(IUnitOfWork repo)
+        {
+            this.repo = repo;
+        }
+
+        public Task<string> Handle(UploadPetPicture request, CancellationToken cancellationToken)
+        {
+            //if (!Directory.Exists(request.FolderPath))
+            //{
+            //    Directory.CreateDirectory(request.FolderPath);
+            //}
+
+            //using (FileStream fileStram = System.IO.File.Create(request.FolderPath + request.FileName))
+            //{
+            //    fileUpload.File.CopyTo(fileStram);
+            //    fileStram.Flush();
+            //    return Ok();
+            //}
+
+            return null;
+        }
+    }
+}
