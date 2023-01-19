d9c03a156a83855695bdcec119843abb75a54533
Simeon Yordanov <lookmanche@gmail.com>
2023-01-19 09:43:02 +0200

    created Appointment class

---
+++ PetCareApp/PetCare.Domain/Entities/Appointment.cs
@@ -0,0 +1,14 @@
+﻿namespace PetCare.Domain.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public DateTime DateOfAppointment { get; set; }
+
+        public string ReasonForAppointment { get; set; } = null!;
+
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; } = null!;
+    }
+}
