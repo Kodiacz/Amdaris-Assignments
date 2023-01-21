1af93bbdec7b9e6140a1c943bad8a0a3c903d426
Simeon Yordanov <lookmanche@gmail.com>
2023-01-21 12:16:33 +0200

    added Divider components

--- PetCareApp/PetCare-FE/src/components/User/MyAccountSideBar.js
+++ PetCareApp/PetCare-FE/src/components/User/MyAccountSideBar.js
@@ -1,20 +1,28 @@
+import { Divider } from '@mui/material'
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import './MyAccountSideBar.css'
 
 const MyAccountSideBar = () => {
   return (
+    <>
     <div className='my-account-page'>
       <div className="sidenav">
+        <Divider/>
         <Link to="my-pets">Pets</Link>
+        <Divider/>
         <Link to="create-pet">Add Pet</Link>
-        <Link to="somewhere">Clients</Link>
-        <Link to="somewhere">Contact</Link>
+        <Divider/>
+        <Link to="account-details">My Account</Link>
+        <Divider/>
+        <Link to="my-appointments">My Appointments</Link>
+        <Divider/>
       </div>
       <div className='outlet-container'>
         <Outlet />
       </div>
     </div>
+    </>
   )
 }
 
