4a71ed8b580cd78309f2c5959e316f901319cb0f
Simeon Yordanov <lookmanche@gmail.com>
2023-01-21 12:16:45 +0200

    wokring on UserSettings

---
+++ PetCareApp/PetCare-FE/src/components/User/UserSettings.js
@@ -0,0 +1,11 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+import './UserSettings.css'
+
+const UserSettings = () => {
+    return (
+        <></>
+    )
+}
+
+export default UserSettings
