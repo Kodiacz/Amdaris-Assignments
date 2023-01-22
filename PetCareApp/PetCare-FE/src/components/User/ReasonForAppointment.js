ac71ea48f0c30494880b93c74e846c912b349c17
Simeon Yordanov <lookmanche@gmail.com>
2023-01-22 16:19:49 +0200

    created new component

---
+++ PetCareApp/PetCare-FE/src/components/User/ReasonForAppointment.js
@@ -0,0 +1,20 @@
+import { TextField } from "@mui/material"
+import { useCallback } from "react"
+import './ReasonForAppointment.css'
+
+const ReasonForAppointment = ({
+    setResultFromInput,
+}) => {
+    const getValue = useCallback(e => {
+        setResultFromInput(e.target.value)
+      }, [setResultFromInput])
+    
+    return (
+        <div className="reason-for-appointment-container">
+            <label className="reason-for-appointment-label">Reason For Appointment</label>
+            <TextField multiline={true} minRows={9} fullWidth={true} onBlur={(e) => getValue(e)} />
+        </div>
+    )
+}
+
+export default ReasonForAppointment
