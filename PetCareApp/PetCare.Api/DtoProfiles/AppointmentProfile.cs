b9e36c9059570848c5ad697c7d04124ffb2e19d8
Simeon Yordanov <lookmanche@gmail.com>
2023-01-23 18:29:19 +0200

    removed additional ForMember method

--- PetCareApp/PetCare.Api/DtoProfiles/AppointmentProfile.cs
+++ PetCareApp/PetCare.Api/DtoProfiles/AppointmentProfile.cs
@@ -7,8 +7,7 @@
         public AppointmentProfile()
         {
             CreateMap<CreateAppointmentDto, CreateAppointmen>();
-            CreateMap<Appointment, GetAppointmentDto>()
-                .ForMember(gad => gad.OwnerFullName, opt => opt.MapFrom(src => src.Owner.FirstName + " " + src.Owner.LastName));
+            CreateMap<Appointment, GetAppointmentDto>();
 
         }
     }
