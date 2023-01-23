de548459068e2dd5a1daa7f8e59d2ab3a2ddd88a
Simeon Yordanov <lookmanche@gmail.com>
2023-01-23 11:40:14 +0200

    created more states for child components

--- PetCareApp/PetCare-FE/src/Pages/DoctorPages/DoctorDetails.js
+++ PetCareApp/PetCare-FE/src/Pages/DoctorPages/DoctorDetails.js
@@ -7,21 +7,34 @@ import * as ownerServices from "../../services/ownerServcies"
 import ReasonForAppointment from "../../components/User/ReasonForAppointment"
 import { AuthContext } from '../../contexts/AuthContext'
 import { Newspaper } from "@mui/icons-material"
-import { Button } from "@mui/material"
+import AddIcon from '@mui/icons-material/Add';
+import { Alert, Button, Divider } from "@mui/material"
+import Alerts from "../../components/Alert/Alerts"
 
 const DoctorDetails = ({
     doctor,
 }) => {
+    const [pet, setPet] = useState();
+    const { user } = useContext(AuthContext);
+    const [rerender, setRerender] = useState({});
+    const [newSchedule, setNewSchedule] = useState();
     const [doctorSchedule, setDoctorSchedule] = useState([]);
     const [resultFromInput, setResultFromInput] = useState();
-    const [newSchedule, setNewSchedule] = useState({});
-    const { user } = useContext(AuthContext);
-    console.log(newSchedule);
+    const [alertState, setAlertState] = useState({
+        openValue: false,
+        severityValue: '',
+        titleValue: '',
+        titleType: '',
+    })
+
     useEffect(() => {
+        console.log('use effect')
         doctorsServices.getDoctorSchedule(doctor.id)
             .then(res => setDoctorSchedule(res))
             .catch(err => console.log(err))
-    }, [])
+    }, [rerender])
+
+    const disableButton = () => (user.username && newSchedule && resultFromInput && pet) ? false : true
 
     const scheduleDays = [...doctorSchedule].map(d => (
         {
@@ -36,27 +49,63 @@ const DoctorDetails = ({
 
     let availableDays = scheduleDays.filter((sd) => sd.isAvailable)
 
-    const fetchData = () => {
-        debugger;
-        const updateScheduleResult = doctorsServices.updateDoctorSchedule(newSchedule, user.accessToken)
+    const fetchData = async () => {
+        // const updateScheduleResult = await doctorsServices.updateDoctorSchedule(newSchedule, user.accessToken)
 
-        const appointmentData = {
-            date: new Date().toLocaleDateString(),
-            reasonForAppointment: resultFromInput,
-            ownerId: user.userId,
-        }
-        const getchAddAppointmentResult = ownerServices.updateOwnerAppointments(appointmentData, user.accessToken)
+        // const appointmentData = {
+        //     dateOfAppointment: newSchedule.date,
+        //     reasonForAppointment: resultFromInput,
+        //     ownerId: user.userId,
+        // }
+
+        // const fetchAddAppointmentResult = await ownerServices.updateOwnerAppointments(appointmentData, user.accessToken);
+        // setRerender(fetchAddAppointmentResult);
+        // console.log('fetchData', fetchAddAppointmentResult)
+        setAlertState(state => {
+            return {
+                openValue: true,
+                severityValue: 'success',
+                titleValue: 'You have made an appointment for this date successfully',
+                titleType: 'Success'
+            }
+        })
     }
-
+    console.log(alertState)
     return (
         <>
-            <ReasonForAppointment input={resultFromInput} setResultFromInput={setResultFromInput} />
+            {
+                user.username
+                    ? <ReasonForAppointment setPet={setPet} input={resultFromInput} setResultFromInput={setResultFromInput} />
+                    : ""
+            }
             <div className="doctor-calendar">
-                <Calendar availableDays={availableDays} setNewSchedule={setNewSchedule} />
+                <Alerts 
+                openValue={alertState.openValue} 
+                severityValue={alertState.severityValue} 
+                titleValue={alertState.titleValue}
+                titleType={alertState.titleType} 
+                closeAlert={setAlertState} />
+                <Calendar availableDays={availableDays} setNewSchedule={setNewSchedule} setAlertState={setAlertState} />
                 <div className="make-appointment-div">
-                    <button type='submit' className="make-appointent-button" size="large" value={'Make Appointment'} onClick={fetchData}>
+                    {
+                        user.username
+                            ? <Button
+                                startIcon={<AddIcon />}
+                                disabled={disableButton()}
+                                variant="outlined"
+                                type='submit'
+                                className="make-appointent-button"
+                                size="large"
+                                value={'Make Appointment'}
+                                onClick={fetchData}
+                            >
+                                Make Appointment
+                            </Button>
+                            : ""
+                    }
+                    {/* <button type='submit' className="make-appointent-button" size="large" value={'Make Appointment'} onClick={fetchData}>
                         Make Appointment
-                    </button>
+                    </button> */}
                 </div>
             </div>
             <div className="detailed-doctor">
