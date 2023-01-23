e359d9bc4185ecde5e24bd90ae3e45d0f7733d8c
Simeon Yordanov <lookmanche@gmail.com>
2023-01-23 18:27:16 +0200

    created MyAppointments page

---
+++ PetCareApp/PetCare-FE/src/Pages/AccountPages/MyAppointments.js
@@ -0,0 +1,83 @@
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableRow from '@mui/material/TableRow';
+import { styled } from '@mui/material/styles';
+import TableBody from '@mui/material/TableBody';
+import TableHead from '@mui/material/TableHead';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import TableContainer from '@mui/material/TableContainer';
+import * as ownerServcies from '../../services/ownerServcies';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(petName, doctorName, reason, date) {
+  return { petName, doctorName, reason, date, };
+}
+
+
+export default function MyAppointments() {
+    const [rows, setRows] = useState();
+    const [appointents, setAppointments] = useState([]);
+    const { user }= useContext(AuthContext);
+
+    useEffect(() => {
+      fetchData(user.userId, user.accessToken)
+    }, []);
+    
+    
+    const fetchData = async (userId, accessToken) => {
+       const getOwnerAppointmentsResult = await ownerServcies.getOwnerAppointments(userId, accessToken);
+
+       setAppointments(state => {
+        return [
+            ...state,
+            ...getOwnerAppointmentsResult,
+        ]
+       })
+    }
+    console.log(appointents)
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 550 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Pet Name</StyledTableCell>
+            <StyledTableCell align="left">Doctor Name</StyledTableCell>
+            <StyledTableCell align="left">Reason</StyledTableCell>
+            <StyledTableCell align="left">Date</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {appointents?.map(a => (
+            <StyledTableRow key={a.name}>
+              <StyledTableCell component="th" scope="row">{a.petName}</StyledTableCell>
+              <StyledTableCell align="left">{a.doctorFullName}</StyledTableCell>
+              <StyledTableCell align="left">{a.reasonForAppointment}</StyledTableCell>
+              <StyledTableCell align="left">{a.dateOfAppointment.slice(0, 10)}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
