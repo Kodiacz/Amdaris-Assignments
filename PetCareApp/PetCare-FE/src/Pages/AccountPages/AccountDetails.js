6ac57f01e80a39f23bbb78271de1d4a45058d4ae
Simeon Yordanov <lookmanche@gmail.com>
2023-01-25 22:33:30 +0200

    working on nested route page

--- PetCareApp/PetCare-FE/src/Pages/AccountPages/AccountDetails.js
+++ PetCareApp/PetCare-FE/src/Pages/AccountPages/AccountDetails.js
@@ -9,6 +9,15 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import * as ownerServices from '../../services/ownerServcies'
 import { AuthContext } from '../../contexts/AuthContext'
+import { Link, Outlet } from 'react-router-dom';
+
+function changeStateValue(callBack, value) {
+    callBack(state => value);
+}
+
+const tipografyStyle = {
+    padding: '10px 0'
+}
 
 export default function AccountDetails() {
     const [owner, setOwner] = useState({})
@@ -22,7 +31,6 @@ export default function AccountDetails() {
                 ...getOwnerResult
             }
         })
-
     }
 
     useEffect(() => {
@@ -31,7 +39,7 @@ export default function AccountDetails() {
 
     useEffect(() => {
         if (owner.profileImageFilePath) {
-            setProfilePicture( owner.profileImageFilePath);
+            changeStateValue(setProfilePicture, owner.profileImageFilePath)
         }
     }, [owner])
 
@@ -54,44 +62,65 @@ export default function AccountDetails() {
         }
     }
 
-    return (
-        <div className='account-details-container'>
-            <Paper elevation={24}>
+    const navigateToEdit = () => {
 
-                <Box sx={{ minWidth: 275 }}>
-                    <Card variant="outlined">
-                        <CardContent>
-                            <Typography variant='h5'>
-                                {`First and Last name: ${owner.firstName} ${owner.lastName}`}
-                            </Typography>
-                            <Divider />
-                            <Typography variant='h5'>
-                                {`Email: ${owner.email}`}
-                            </Typography>
-                            <Divider />
-                            <Typography variant='h5'>
-                                Phonenumber: {`${owner.phonenumber}`}
-                            </Typography>
-                            <Divider />
-                        </CardContent>
-                        <CardActions>
-                            <ButtonGroup
-                                disableElevation
-                                variant="contained"
-                                aria-label="Disabled elevation buttons"
-                            >
-                                <form method='post' onSubmit={(e) => handleUpload(e)}>
-                                    <TextField
-                                        type='file'
-                                        name='uploadPicture'
-                                    />
-                                    <Button type='submit' size='large'>Upload</Button>
-                                </form>
-                            </ButtonGroup>
-                        </CardActions>
-                    </Card>
-                </Box>
-            </Paper>
-        </div>
+    }
+
+    return (
+        <>
+            <div className='account-details-container'>
+                <Paper elevation={24}>
+                    <Box sx={{ minWidth: 275 }}>
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography sx={tipografyStyle} variant='h5'>
+                                    First and Last name:
+                                </Typography>
+                                <Typography variant='h5'>
+                                    {`${owner.firstName} ${owner.lastName}`}
+                                </Typography>
+                                <Divider />
+                                <Typography sx={tipografyStyle} variant='h5'>
+                                    Email:
+                                </Typography>
+                                <Typography variant='h6'>
+                                    {`${owner.email}`}
+                                </Typography>
+                                <Divider />
+                                <Typography sx={tipografyStyle} variant='h5'>
+                                    Phonenumber:
+                                </Typography>
+                                <Typography variant='h5'>
+                                    {`${owner.phonenumber}`}
+                                </Typography>
+                                <Divider />
+                            </CardContent>
+                            <CardActions>
+                                <ButtonGroup
+                                    disableElevation
+                                    variant="contained"
+                                    aria-label="Disabled elevation buttons"
+                                >
+                                    <form method='post' onSubmit={(e) => handleUpload(e)}>
+                                        <TextField
+                                            type='file'
+                                            name='uploadPicture'
+                                        />
+                                        <Button type='submit' size='large'>Upload</Button>
+                                    </form>
+                                </ButtonGroup>
+                            </CardActions>
+                        </Card>
+                        {/* <Button element={<Link />} to="edit-account" variant="contained" color="primary">
+                        Edit Account
+                    </Button> */}
+                        <Link to='edit-account'>Test</Link>
+                    </Box>
+                </Paper>
+            </div>
+            <div className='edit-user-outlet-div'>
+                <Outlet />
+            </div>
+        </>
     );
 }
